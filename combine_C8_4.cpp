ef7e4792c1fc472188ccd2f575bbccde49cf7db7
shuiyd <2503417117@qq.com>
2020-04-20 17:36:18 +0800

    Update combine_C8_4.cpp

--- combine_C8_4.cpp
+++ combine_C8_4.cpp
@@ -3,38 +3,34 @@
 #include<iostream>
 using namespace std;
 
-void CombinationCore(int* str, int len, vector<int> &rs, int nlen)
-{
-	if(len == 0)
-	{
-        if(rs.size() == nlen ){
-            vector<int>::iterator it;
-            for(it = rs.begin(); it < rs.end(); it++) {
-                cout<<(*it) << ",";
-            }
-            cout<<endl;
-        }
-		return;
-	}
-	if(*str == '\0')
-	return;
-	rs.push_back(*str);
-	CombinationCore(str + 1,len - 1,rs, nlen);
-	rs.pop_back();
-	CombinationCore(str + 1, len,rs, nlen);
+
+void swap(vector<int> &vec, int i, int j){
+    int tmp = vec[i];
+    vec[i] = vec[j];
+    vec[j] = tmp;
 }
-void getCombination(int* str,int length, int nlen)
-{
-	if(!str ) {
+
+void permute(vector<int>& vec, int start){
+    if(start == vec.size()-1){
+        for(int i=0;i<vec.size();++i){
+            cout << vec[i] <<",";
+        }
+        cout<<endl;
         return;
     }
-	vector<int> rs;
-	for(int i = 1; i <= length; i++) {
-        CombinationCore(str,i,rs, nlen);
+
+    for(int i=start;i<vec.size();++i){
+        swap(vec, i, start);
+        getComb(vec, i+1 );
+        swap(vec, i, start);
     }
 }
+
 int main()
 {
-    int ary[] = {18, 16, 15, 14, 8, 7, 3, 1};
-	getCombination(ary, 8, 4);
+    vector<int> vec;
+    for(int i=0;i<5;i++){
+        vec.push_back(i);
+    }
+    permute(vec,0);
 }
